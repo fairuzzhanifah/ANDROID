80961bb9f94a46b62b7b19203406eb0dd0c2972b
ichsanulabid <ichsanulabid.08@gmail.com>
2024-05-23 13:30:51 +0800

    First commit

---
+++ app/src/main/java/com/example/eduvent/MainActivity.kt
@@ -0,0 +1,10 @@
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomePageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_page)
+    }
+}
